d62b8ec5e16907eea9dfe03a54ea4d9cc34f4e84
SadMearise <65180406+SadMearise@users.noreply.github.com>
2022-07-28 14:57:39 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
-// import "./modules/swiper-bundle.min.js";
-// import "./modules/swiper-options.js";
 
 flsFunctions.isWebp();
 flsFunctions.ibg();
+flsFunctions.stepForm();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -20,3 +20,61 @@ export function ibg() {
         }
     }
 }
+
+export function stepForm() {
+	const form = document.querySelector('.registration__form');
+	const finish = document.querySelector('.finish');
+	const steps = document.querySelectorAll('.form__step');
+	const prevBtn = document.querySelector('.form__btn-prev');
+	const nextBtn = document.querySelector('.form__btn-next');
+	const progressStep = document.querySelectorAll('.progress__step');
+	const progressSuccess = document.querySelector('.progress__success');
+	
+	form.addEventListener('submit', (e) => e.preventDefault())
+
+	let formStepIndex = 0;
+
+	prevBtn.addEventListener('click', () => {
+		formStepIndex--;
+
+		progressStep[formStepIndex + 1].classList.remove('active');
+
+		updateformSteps();
+	})
+
+	nextBtn.addEventListener('click', () => {
+		if (formStepIndex < steps.length - 1) {
+			formStepIndex++;
+			updateformSteps();
+		} else if (formStepIndex === steps.length - 1) {
+			form.style.display = 'none';
+			finish.style.display = 'block';
+		}
+	})
+
+	function updateformSteps() {
+		steps.forEach(step => {
+			step.classList.contains('active') && step.classList.remove('active');
+		})
+
+		steps[formStepIndex].classList.add('active');
+
+		if (formStepIndex <= progressStep.length - 1) {
+			progressStep[formStepIndex].classList.add('active');
+		}
+
+		if (formStepIndex === 0) {
+			prevBtn.style.display = 'none';
+		} else {
+			prevBtn.style.display = 'block';
+		}
+
+		const actives = document.querySelectorAll('.progress__step.active');
+		const percent = ((actives.length - 1) / (progressStep.length - 1)) * 100 + "%";
+		progressSuccess.style.width = percent;
+	}
+
+	if (steps) {
+		updateformSteps();
+	}
+}
